6f52e1da1a719b8a32f4fd2048a8ccea7c8b605f
Franco <soporte@infinitumscitech.com>
2019-12-23 09:45:18 -0500

    primer commit

---
+++ server/server.js
@@ -0,0 +1,55 @@
+///////////////////////////
+//   SERVER JS
+//////////////////////////
+require('./config/config');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+// app.get('/', function(req, res) {
+//     res.json('hello world');
+// })
+
+app.get('/usuario', function(req, res) {
+    res.json('get usuario');
+});
+
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+    if (body.nombre === undefined) {
+
+        res.status(400).json({
+            ok: false,
+            mensaje: 'el nombre es necesario',
+
+        });
+
+    } else {
+
+        res.json({
+            persona: body
+        });
+    }
+});
+
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuario', function(req, res) {
+    res.json('delete usuario');
+});
+app.listen(3000, () => {
+    console.log('escuchando el puerto: ', process.env.PORT);
+});
